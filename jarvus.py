41e044327e7c9d825df701111d9427ba201be176
dadiletta <dan.adiletta@gmail.com>
2017-03-05 22:05:57 -0500

    Using xset for screen toggle

--- jarvus.py
+++ jarvus.py
@@ -34,7 +34,7 @@ class Jarvus(App):
 
     def turn_screen_on(self):
         try:
-            bash_command = "./screen_on.sh"
+            bash_command = "sudo ./screen_on.sh"
             subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
             self.screen_on = True
         except Exception as ee:
@@ -42,7 +42,7 @@ class Jarvus(App):
 
     def turn_screen_off(self):
         try:
-            bash_command = "./screen_off.sh"
+            bash_command = "sudo ./screen_off.sh"
             subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
             self.screen_on = False
         except Exception as ee:
